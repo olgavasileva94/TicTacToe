e5d7e7136a09eb2930ba6c1052d63b66f8c73361
Ольга Васильева <stereomatrass@mail.ru>
2018-05-20 12:46:48 +0700

    Изменения в методах SetNamePlayer1, SetNamePlayer2

--- ClassLibraryTicTacToe/LibraryTicTacToe.cs
+++ ClassLibraryTicTacToe/LibraryTicTacToe.cs
@@ -29,7 +29,7 @@ namespace ClassLibraryTicTacToe
 
         public void SetNamePlayer1(String Name)
         {
-            this.NamePlayer1 = "Olga1";
+            this.NamePlayer1 = Name;
         }
 
         public String GetNamePlayer2()
@@ -39,7 +39,7 @@ namespace ClassLibraryTicTacToe
 
         public void SetNamePlayer2(String Name)
         {
-            this.NamePlayer2 = "Olga2";
+            this.NamePlayer2 = Name;
         }
     }
 }
